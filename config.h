f1dfc9e7b3ef53ac204ddd03175fc5dbd2851a4e
Stephen Parker <parrotpirate@gmail.com>
2022-09-19 17:37:40 -0400

    tweak config files

--- config.h
+++ config.h
@@ -2,6 +2,7 @@
   Set any config.h overrides for your specific keymap here.
   See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
 */
+#pragma once
 #define ORYX_CONFIGURATOR
 #define IGNORE_MOD_TAP_INTERRUPT
 #undef TAPPING_TERM
@@ -16,8 +17,10 @@
 #define NO_AUTO_SHIFT_NUMERIC
 #define CAPS_LOCK_STATUS
 #define HOLD_ON_OTHER_KEY_PRESS
-#define FIRMWARE_VERSION u8"r5KK9/RPyr5"
-#define RAW_USAGE_PAGE 0xFF60
-#define RAW_USAGE_ID 0x61
 #define LAYER_STATE_8BIT
 #define RGB_MATRIX_STARTUP_SPD 60
+
+#undef DEBOUNCE_DELAY
+#define DEBOUNCE_DELAY 5
+
+#define COMBO_COUNT 1
